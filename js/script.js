aa73fb4b9ca1873482f4074bb335f701496ea728
Nelkeys <nelsonekoh212@gmail.com>
2024-07-04 21:10:15 +0000

    New Website Done

--- js/script.js
+++ js/script.js
@@ -1,28 +1,2 @@
-const icon = document.querySelector('.floating-message i');
-const message = document.querySelector('.floating-message p');
 
-icon.addEventListener('mouseover', () => {
-    message.style.display = 'block';
-});
-
-icon.addEventListener('mouseout', () => {
-    message.style.display = 'none';
-});
-
-
-document.querySelectorAll('.copy-address').forEach(copyButton => {
-    copyButton.addEventListener('click', function () {
-        const address = this.closest('.qr-box').querySelector('.bchain-address b').innerText;
-        navigator.clipboard.writeText(address).then(() => {
-            this.innerHTML = 'Copied <i class="bx bx-check"></i>';
-            this.classList.add('copied');
-            setTimeout(() => {
-                this.innerHTML = 'Copy wallet address <i class="bx bx-copy"></i>';
-                this.classList.remove('copied');
-            }, 2000);
-        }).catch(error => {
-            console.error('Error copying text: ', error);
-        });
-    });
-});
 
--- js/stopstealing.js
+++ js/stopstealing.js
@@ -1,7 +1,10 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+/**
 document.onkeydown = function(e) {
     if (e.keyCode == 123) { // F12 key
         return false;
     }
     
 }
+ */
